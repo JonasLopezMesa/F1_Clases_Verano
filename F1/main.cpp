1ce9a28c6f67e7002977a3cf46c08d12f8e40f17
JonasLopezMesa <77jonas77@gmail.com>
2014-07-03 11:39:13 +0100

    Initial Commit

---
+++ F1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  F1
+//
+//  Created by Jonás López Mesa on 03/07/14.
+//  Copyright (c) 2014 Jonás López Mesa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
